c7e06bc1a34122d88b4cd2710e20396584b6cbca
Y2FsbCBtZSBQcm9mZXNzb1Ig <47165982+D3vj0e@users.noreply.github.com>
2021-01-08 22:03:29 +0700

    upload file python
    01 upload non fix

---
+++ main.py
@@ -0,0 +1,14 @@
+
+url = "http://206.189.118.190:30277/"
+import requests
+import hashlib
+from bs4 import BeautifulSoup
+
+session = requests.session()
+r = session.get(url)
+soup = BeautifulSoup(r.text,"html.parser")
+plan_text = soup.h3.text
+cipher_text = hashlib.md5(plan_text.encode())
+post_param = {"hash":cipher_text.hexdigest()}
+final_res = session.post(url,post_param)
+print(final_res.text)
